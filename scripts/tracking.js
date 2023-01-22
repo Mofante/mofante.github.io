aaad9eb54378c0d0c2654ac64b9d7b6730a407c2
mrmejer <m.j.mejer@student.tudelft.nl>
2023-01-22 20:26:57 +0100

    Styled tracking box and combined two scripts.

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -5,14 +5,16 @@ let charactersTyped = 0;
 
 time = Date.now();
 
-document.getElementById("register").onclick = () => {
+let showTrackingData = () => {
     document.getElementById("behavioral-data").innerHTML = 
-    "Time spent on this website: " + Math.floor((Date.now() - time) / 1000) + "s" +
-    "Clicks " + mouseClicks +
-    "Key presses" + keyPresses +
-    "Characters typed " + charactersTyped;
+    "<h1>Behavioral data</h1>" +
+    "Time spent on this website: " + Math.floor((Date.now() - time) / 1000) + "s<br>" +
+    "Clicks: " + mouseClicks + "<br>" +
+    "Key presses: " + keyPresses + "<br>" +
+    "Characters typed: " + charactersTyped;
+
     document.getElementById("behavioral-data").style.display = "block";
-}
+};
 
 for(let e of document.querySelectorAll("input")) {
     e.addEventListener("keypress", (event) => {
